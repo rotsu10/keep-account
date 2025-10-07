e095c37ea9f540e1defdb876536b35c47a85ab9a
rotsu10 <3242992897@qq.com>
2025-10-07 16:03:36 +0800

    检测git

--- demo_back/back/server/src/main/java/com/example/Application.java
+++ demo_back/back/server/src/main/java/com/example/Application.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        log.info("server started");
+        log.info("serv1132er started");
 
     }
 }
