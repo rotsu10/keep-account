9b4dcaf15bc99957cc0637b61768931f80ac4038
rotsu10 <3242992897@qq.com>
2025-10-08 15:05:26 +0800

    前端登录

--- demo_front/pages/common/login.vue
+++ demo_front/pages/common/login.vue
@@ -5,37 +5,77 @@
 		</view>
 		<view class="login">
 			<van-cell-group inset>
-			  <van-field v-model="value" label="文本" placeholder="请输入用户名" />
+			  <van-field v-model="username" label="用户名" placeholder="请输入用户名" />
 			  <van-field v-model="password" type="password" label="密码" />
 			</van-cell-group>
 		</view>
 		<view class="button_login">
-			<van-button round type="primary" size="normal" to="/pages/home/home">登录</van-button>
+			<van-button round type="primary" size="normal" @click="login">登录</van-button>
 		</view>
 	</view>
 	
 </template>
 
 <script>
-	import { ref } from 'vue';
-	export default {
-		data() {
-			return {
-				title: 'Hello，欢迎使用'
-			}
-		},
-		setup() {
-		    const value = ref('');
-			 const password = ref('');
-		    return { value,password };
-		},
-		onLoad() {
+import { ref } from 'vue';
+export default {
+  setup() {
+    const title = ref('Hello，欢迎使用');
+    const username = ref('');
+    const password = ref('');
 
-		},
-		methods: {
+	//登录
+    const login = () => {
+      // 非空校验
+      if (!username.value) {
+        uni.showToast({ title: '请输入用户名', icon: 'none' });
+        return; 
+      }
+      if (!password.value) {
+        uni.showToast({ title: '请输入密码', icon: 'none' });
+        return;
+      }
+    
+      uni.request({
+        url: 'http://localhost:8080/user/login', 
+        method: 'POST',
+        header: { 'Content-Type': 'application/json' },
+        data: {
+          username: username.value,
+          password: password.value,
+        },
+        success: (res) => {
+          const { statusCode, data: result } = res;
+          
+          if (statusCode !== 200) {
+            uni.showToast({ title: `请求失败, ${statusCode}`, icon: 'none' });
+            return;
+          }
+    
+          if (!result) {
+            uni.showToast({ title: "接口返回空数据", icon: "none" });
+            return;
+          }
+    
+          if (result.code === 1) {
+            uni.showToast({ title: "登录成功" });
+            uni.setStorageSync('token', result.data.token); 
+            uni.redirectTo({ url: '/pages/home/home' });
+          } else {
+            uni.showToast({ title: result.message, icon: 'none' });
+          }
+        },
+        fail: (error) => {
+          console.error('请求失败:', error);
+          uni.showToast({ title: '网络错误，请检查跨域和后端服务', icon: 'none' });
+        }
+      });
+    };
 
-		}
-	}
+    // 必须将变量和方法返回给模板
+    return { title, username, password, login };
+  }
+}
 </script>
 
 <style>
