bbff03f8ec3a87fd8c94295c7fc7f13ec5945a56
rotsu10 <3242992897@qq.com>
2025-11-15 14:39:44 +0800

    优化时间范围和类型选择器

--- demo_front/components/TimeRange.vue
+++ demo_front/components/TimeRange.vue
@@ -1,17 +1,11 @@
 <template>
 	<view class="selected">
 		<van-row gutter="20" justify="center">
-			<van-col span="10">
-				<van-field v-model="timeValue" is-link readonly label="时间范围"
-					@click="openPicker('time')" />
-			</van-col>
-			<van-col span="10">
-				<van-field v-model="typeValue" is-link readonly label="类型"
-					@click="openPicker('type')" />
-			</van-col>
-			
+			<van-field v-model="fieldValue" is-link readonly label="选择"  placeholder="选择时间和范围" @click="showPicker = true" />
+
 			<van-popup v-model:show="showPicker" round position="bottom">
-				<van-picker :columns="currentColumns" @cancel="showPicker = false" :default-index="0" @confirm="onConfirm" />
+				<van-picker :columns="columns" @cancel="showPicker = false"
+					@confirm="onConfirm" />
 			</van-popup>
 		</van-row>
 	</view>
@@ -22,11 +16,10 @@
 </template>
 
 <script setup>
-	import {
-		computed,
-		ref
-	} from 'vue';
-
+	import {computed,ref} from 'vue';
+	
+	const emit = defineEmits(['time-range']);
+	
 	const getCurDate = () => {
 		const now = new Date();
 		return [
@@ -37,70 +30,53 @@
 	};
 
 	const getCurDateArr = ref(getCurDate());
-	const columnsTime = [{
-			text: '本年',
-			value: getCurDateArr.value[0]
-		},
-		{
-			text: '本月',
-			value: getCurDateArr.value[1]
-		},
-		{
-			text: '本日',
-			value: getCurDateArr.value[2]
-		}
-	];
-	const columnsType = [{
-			text: '收入',
-			value: '1'
-		},
-		{
-			text: '支出',
-			value: '2'
-		},
-		{
-			text: '转账',
-			value: '3'
-		}
-	];
-	
-	const showPicker = ref(false);
-	const currentField = ref('');
-	const timeValue = ref('');
-	const typeValue = ref('');
+	const columns = [
+		[{
+				text: '本年',
+				value: getCurDateArr.value[0]
+			},
+			{
+				text: '本月',
+				value: getCurDateArr.value[1]
+			},
+			{
+				text: '本日',
+				value: getCurDateArr.value[2]
+			}
+		],
+		[{
+				text: '收入',
+				value: '1'
+			},
+			{
+				text: '支出',
+				value: '2'
+			},
+			{
+				text: '转账',
+				value: '3'
+			}
+		]
+	]
 
-	const currentColumns = computed(()=>{
-		if(currentField.value === 'time'){
-			return columnsTime;
-		}else if(currentField.value === 'type'){
-			return columnsType;
-		}
-		return [];
-	})
-	
-	const openPicker = (field) => {
-		currentField.value = field;
-		showPicker.value = true;
+	const showPicker = ref(false);
+	const fieldValue = ref('');
+	const onConfirm = ({
+		selectedOptions
+	}) => {
+		console.log("选择数据为：",selectedOptions)
+		sendToParent(selectedOptions);
+		showPicker.value = false;
+		fieldValue.value = `${selectedOptions[0].text} / ${selectedOptions[1].text}`;
 	};
 	
-	const onConfirm = ({ selectedOptions }) => {
-		showPicker.value = false;
-		const selected = selectedOptions[0];
-
-		if (currentField.value === 'time') {
-			timeValue.value = selected.text;
-			console.log("选择的时间范围:", selected.value);
-		} else if (currentField.value === 'type') {
-			typeValue.value = selected.text;
-			console.log("选择的类型:", selected.value);
-		}
+	const sendToParent = (selectedOptions) => {
+	  emit('time-range', {
+		  time: selectedOptions[0],
+		  type: selectedOptions[1]
+	  }); 
 	};
 </script>
 
 <style scoped>
-	:deep(.van-field) {
-	  margin-right: 0 !important;
-	  padding-right: 0 !important;
-	}
-
 </style>
--- demo_front/pages/mine_list/categoryStatistics.vue
+++ demo_front/pages/mine_list/categoryStatistics.vue
@@ -1,15 +1,40 @@
 <template>
-	<view>
-		<TimeRangeVue></TimeRangeVue>
-		<categoryChartVue></categoryChartVue>
+	<view class="selected">
+		<TimeRangeVue @time-range="handleTimeRangeSelect"></TimeRangeVue>
 	</view>
+  <div ref="chartRef" style="width: 100%; height: 400px;"></div>
 </template>
 
 <script setup>
-	import TimeRangeVue from '../../components/TimeRange.vue';
-	import categoryChartVue from '../../components/categoryChart.vue';
+import { ref, onMounted } from 'vue';
+import * as echarts from 'echarts';
+import TimeRangeVue from '../../components/TimeRange.vue';
+const selectedTime = ref('');
+const selectedType = ref('');
+
+const handleTimeRangeSelect = (data) => {
+  console.log("父组件收到数据:", data);
+  selectedTime.value = data.time.text;
+  selectedType.value = data.type.text;
+};
+
+const chartRef = ref(null);
+onMounted(() => {
+  const myChart = echarts.init(chartRef.value);
+  let option = {
+    title: { text: '圆环图的例子', left: 'center', top: 'center' },
+    series: [
+      {
+        type: 'pie',
+        data: [
+          { value: 335, name: 'A' },
+          { value: 234, name: 'B' },
+          { value: 1548, name: 'C' }
+        ],
+        radius: ['40%', '70%'] // 圆环图：内半径40%，外半径70%
+      }
+    ]
+  };
+  myChart.setOption(option);
+});
 </script>
-
-<style>
-
-</style>
